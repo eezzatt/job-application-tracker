098a49040cdadef663567d426c382566c8349877
eezzatt <izzatmuhaimin10@gmail.com>
2026-06-16 21:20:10 +0800

    Added login and registration

--- server.js
+++ server.js
@@ -3,34 +3,35 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const pool = require('./db')
+const authRoutes = require('./auth')
 
 app.use(express.json())
+app.use('/api', authRoutes)
 
 app.get('/', (req, res) => {
     res.send("Job Application Tracker API is running.")
 })
 
-app.post('/insert', authenticateToken, async (req, res) => {
-    const { position, company } = req.body
-    const { userID } = req.user
-
-    if (!position || !company) {
-        return res.status(400).json({ error: "Invalid position or company" })
-    }
-
+app.post('/insert', async (req, res) => {
     try{
+        const { position, company } = req.body
+
+        if (!position || !company) {
+            return res.status(400).json({ error: "Invalid position or company" })
+        }
+
         await pool.query(
             'INSERT INTO jobtracker (user_id, company, position) VALUES ($1, $2, $3)',
-            [userID, company, position]
+            [1, company, position]
         )
 
         res.json({ 
-            user: `${userid}`,
             position: `${position}`,
             company: `${company}`
         })
     }
     catch (err) {
+        console.log(err)
         res.status(500).json({ error: 'Internal server error'})
     }
 })
