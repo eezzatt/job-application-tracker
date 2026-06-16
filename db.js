7ce4093880a9791f58262a82474cdc629a478f02
eezzatt <izzatmuhaimin10@gmail.com>
2026-06-16 21:20:28 +0800

    Added export

--- db.js
+++ db.js
@@ -4,3 +4,5 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_URL === 'production' ? { rejectUnauthorized: false } : false
 })
+
+module.exports = pool
