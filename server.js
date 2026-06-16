40101c2f9f3348925eb0cb4a212505ca5587b990
eezzatt <izzatmuhaimin10@gmail.com>
2026-06-16 14:01:57 +0800

    Added POST method to insert job application

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send("Job Application Tracker API is running.")
+})
+
+app.post('/insert', (req, res) => {
+    const { position, company } = req.body
+
+    if (!position || !company) {
+        return res.status(400).json({ error: "Invalid position or company" })
+    }
+
+    const jobList = {}
+
+    jobList.job = { position, company }
+
+    res.json({ 
+        position: `${position}`,
+        company: `${company}`
+    })
 })
 
 app.listen(3000, () => {
